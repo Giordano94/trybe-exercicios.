30f436554617de2d7d5ebb286c0bfcc386c0713e
Giordano <giordano.mendes94@gmail.com>
2022-05-12 11:14:03 -0300

    adicinando constantes e consoles

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/scripts.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/scripts.js
@@ -1,8 +1,17 @@
-const myName = "Giordano";
-const birthCity = "Araraquara";
-let birthYear = 2030;
+//const myName = "Giordano";
+//const birthCity = "Araraquara";
+//let birthYear = 2030;
 
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+//console.log(myName);
+//console.log(birthCity);
+//console.log(birthYear);
+
+const base = 5;
+const height = 8;
+const area = base + height;
+const peimeter = base * 2 + height * 2;
+
+
+console.log(area)
+console.log(peimeter)
