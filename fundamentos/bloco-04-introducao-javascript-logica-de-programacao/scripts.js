c5d7eb4d8498e2e85f9d73b96eba3c98d516204e
Giordano <giordano.mendes94@gmail.com>
2022-05-12 11:24:43 -0300

    adicinado estrutura if else

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/scripts.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/scripts.js
@@ -7,11 +7,22 @@
 //console.log(birthCity);
 //console.log(birthYear);
 
-const base = 5;
-const height = 8;
-const area = base + height;
-const peimeter = base * 2 + height * 2;
+//const base = 5;
+//const height = 8;
+//const area = base + height;
+//const peimeter = base * 2 + height * 2;
 
 
-console.log(area)
-console.log(peimeter)
+//console.log(area)
+//console.log(peimeter)
+
+const nota = 59
+;
+if (nota > 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+
+} else if (nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+} else {
+    console.log("Você foi reprovada(o)");
+}
