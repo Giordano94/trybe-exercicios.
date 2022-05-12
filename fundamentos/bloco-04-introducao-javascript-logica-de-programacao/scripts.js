28d1017e98b5ca7dcaa8d04c5795cd9b71fdb9a8
Giordano <giordano.mendes94@gmail.com>
2022-05-12 11:44:01 -0300

    adicinado condicionais com AND e OR

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/scripts.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/scripts.js
@@ -16,6 +16,7 @@
 //console.log(area)
 //console.log(peimeter)
 
+/*
 const nota = 59
 ;
 if (nota > 80) {
@@ -25,4 +26,34 @@ if (nota > 80) {
     console.log("Você está na nossa lista de espera");
 } else {
     console.log("Você foi reprovada(o)");
+}
+
+*/
+
+/*const currentHour = 10;
+let message = "";
+
+if (currentHour >= 22) {
+    console.log("Não deveríamos comer nada, é hora de dormir");
+}
+else if (currentHour >= 18 && currentHour < 22) {
+    console.log("Rango da noite, vamos jantar :D");
+}
+else if (currentHour >= 14 && currentHour < 18) {
+    console.log("Vamos fazer um bolo pro café da tarde?");
+}
+else if (currentHour >= 11 && currentHour < 14) {
+    console.log("Hora do almoço!!!");
+}else{
+    console.log("Hmmm, cheiro de café recém passado")
+}
+ */
+
+let weekDay ="sabado"
+
+if(weekDay === "Segunda" || weekDay === "Terça" || weekDay === "Quarta" || weekDay === "Quinta" || weekDay === "Sexta" ){
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+}
+else{
+    console.log("FINALMENTE, descanso merecido UwU")
 }
