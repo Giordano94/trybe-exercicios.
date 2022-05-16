904701fd22b8d6e79f661b3dde81e85d9bcad136
Giordano <giordano.mendes94@gmail.com>
2022-05-16 15:12:13 -0300

    exercicios javascript dia 02

---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,98 @@
+/*
+//Exercicio 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+for(let i = 0; i < numbers.length; i+=1){
+    console.log(numbers[i])
+}
+
+//Exercicio 2
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for(let i = 0; i < numbers.length; i+=1){
+    soma = soma + numbers[i];
+    
+}
+
+console.log(soma);
+
+
+//Exercicio 3
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for(let i = 0; i < numbers.length; i+=1){
+    soma = soma + numbers[i];
+    
+}
+let media = soma / numbers.length;
+
+console.log(media)
+
+console.log(soma);
+
+//Exercicio 4
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for(let i = 0; i < numbers.length; i+=1){
+    soma = soma + numbers[i];
+    
+}
+let media = soma / numbers.length;
+
+console.log(media)
+
+console.log(soma);
+
+if(media > 20){
+    console.log("valor maior que 20")
+}else {
+    console.log("valor menor que 20")
+}
+
+//Exercicio 5
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior = 0;
+
+for(let i = 0; i < numbers.length; i+=1){
+    if(numbers[i] > maior){
+        maior = numbers[i];
+    }
+    
+}
+
+console.log(maior)
+
+//Exercicio 6
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let i = 0; i < numbers.length; i+=1){
+    if(numbers[i] % 2 != 0){
+       console.log(numbers[i])
+    }
+
+}
+
+
+*/
+
+
+
+//Exercicio 7
+
+
+
+
+
