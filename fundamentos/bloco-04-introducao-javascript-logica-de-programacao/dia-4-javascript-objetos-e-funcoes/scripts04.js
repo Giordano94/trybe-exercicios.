3f9cf9005c0051da728fda9c3a9b6d0657369a9c
Giordano <giordano.mendes94@gmail.com>
2022-05-17 18:39:02 -0300

    modificação exercicios 2, 3 e 4,  adiciona exercicio 5

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts04.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts04.js
@@ -9,26 +9,46 @@ let info = {
 
   console.log("Bem vinda, " + info.personagem + "!")
 
-// Exercício 2
+// Exercício 3
 
 
 for( let values in info){
     console.log(values)
 }
 
-// Exercício 3
+// Exercício 4
 
 for( let values in info){
     console.log(info[values])
 }
 
 
+// Exercício 5
 
-// Exercício 1 
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics 178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+
+
+  for (let values  in info){
+    if (
+        values === 'recorrente' &&
+        info[values] === 'Sim' &&
+        info2[values] === 'Sim'
+      ) {
+        console.log('Ambos recorrentes');
+      } else {
+        console.log(info[values] + ' e ' + info2[values]);
+      }
+  }
 
 
 
-// Exercício 1 
+
+// Exercício 6
 
 
 
