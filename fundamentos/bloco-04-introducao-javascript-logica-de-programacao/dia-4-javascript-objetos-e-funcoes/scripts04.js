849f1f55e47a2c7b23d35931b440c832bb34f5b1
Giordano <giordano.mendes94@gmail.com>
2022-05-17 18:21:14 -0300

    exercicios 1, 2 e 3

---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts04.js
@@ -0,0 +1,39 @@
+// Exercício 1 
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: "Sim",
+  };
+
+  console.log("Bem vinda, " + info.personagem + "!")
+
+// Exercício 2
+
+
+for( let values in info){
+    console.log(values)
+}
+
+// Exercício 3
+
+for( let values in info){
+    console.log(info[values])
+}
+
+
+
+// Exercício 1 
+
+
+
+// Exercício 1 
+
+
+
+// Exercício 1 
+
+
+// Exercício 1 
+
