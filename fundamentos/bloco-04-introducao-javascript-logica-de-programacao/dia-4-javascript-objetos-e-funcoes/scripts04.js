9092dba3acd9450c467b0a9b3be97a039c36405e
Giordano <giordano.mendes94@gmail.com>
2022-05-17 18:56:29 -0300

    adiciona exercicios 6, 7 e 8

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts04.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts04.js
@@ -5,20 +5,20 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: "Sim",
-  };
+};
 
-  console.log("Bem vinda, " + info.personagem + "!")
+console.log("Bem vinda, " + info.personagem + "!")
 
 // Exercício 3
 
 
-for( let values in info){
+for (let values in info) {
     console.log(values)
 }
 
 // Exercício 4
 
-for( let values in info){
+for (let values in info) {
     console.log(info[values])
 }
 
@@ -30,30 +30,56 @@ let info2 = {
     origem: 'Christmas on Bear Mountain, Dells Four Color Comics 178',
     nota: 'O último MacPatinhas',
     recorrente: 'Sim',
-  };
+};
 
 
-  for (let values  in info){
+for (let values in info) {
     if (
         values === 'recorrente' &&
         info[values] === 'Sim' &&
         info2[values] === 'Sim'
-      ) {
+    ) {
         console.log('Ambos recorrentes');
-      } else {
+    } else {
         console.log(info[values] + ' e ' + info2[values]);
-      }
-  }
+    }
+}
 
 
 
 
-// Exercício 6
+// Exercício 6 e 7
 
 
 
-// Exercício 1 
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
+        {
+            titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+            autor: 'JK Rowling',
+            editor: 'Rocco',
+        },
+    ],
+};
 
 
-// Exercício 1 
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + leitor.livrosFavoritos[0].titulo)
+console.log("O segundo livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + leitor.livrosFavoritos[1].titulo)
+
+// Exercício 8
+
+console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos.")
+
+
+
+
+
 
