0dc1d9fd44e7b8a0640df2eef076bcd84972517c
Giordano <giordano.mendes94@gmail.com>
2022-06-22 18:27:16 -0300

    exercicios 1 e 2

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/ scriptsBloco8.js
@@ -0,0 +1,29 @@
+// //exercicio 1
+
+// const employeeData = (nomeCompleto) => {
+//   const email = nomeCompleto.toLowerCase().split(" ").join("_");
+//   return { nomeCompleto, email: `${email}@trybe.com` };
+// };
+
+// const newEmployee = (employeeData) => {
+//   const employees = {
+//     id: employeeData("Giordano Mendes"),
+//   };
+//   return employees;
+// };
+
+// console.log(newEmployee(employeeData));
+
+// //Exercicio 2;
+
+// const betting = (betNumber, drawnNumber) => betNumber === drawnNumber;
+
+// const lotery = (drawnNumber, callback) => {
+//   const drawnNumber = Math.floor(Math.random() * 10);
+
+//   return callback(betNumber, drawnNumber)
+//     ? "Lucky day, you won!"
+//     : "Try Again!";
+// };
+
+// Exercicio 3
